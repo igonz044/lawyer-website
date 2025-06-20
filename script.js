b14137c7543d516dc09716d6ff5eb55abe36851d
igonz044 <igonz044@ucr.edu>
2025-06-19 23:49:33 -0700

    Initial commit of Fabian Law Firm website

---
+++ script.js
@@ -0,0 +1,18 @@
+// This is a placeholder for your custom scripts. 
+
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion-btn");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+}); 
